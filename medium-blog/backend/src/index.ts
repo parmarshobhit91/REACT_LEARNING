a3b2559c61c7cc6d209b05e390a6837276da2254
Shobhit <parmarshobhit91@gmail.com>
2024-12-28 00:17:33 +0530

    Holding project

--- medium-blog/backend/src/index.ts
+++ medium-blog/backend/src/index.ts
@@ -1,8 +1,42 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
-app.get('/', (c) => {
+app.post('/api/v1/signup',async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  })
+  return c.text('Hello hono!')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
